7944a9adcf65ef79886df4b33a46ed364203000b
Tanapon   Ninket <31594062+BomNk@users.noreply.github.com>
2018-02-23 01:29:03 +0700

    PC

--- AnalogInput/AnalogInput.ino
+++ AnalogInput/AnalogInput.ino
@@ -26,52 +26,95 @@
 
   http://www.arduino.cc/en/Tutorial/AnalogInput
 */
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library. 
 
-int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;
+const int PulseWire = A0;   // select the input pin for the potentiometer
+//const int PulseWire = A0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;
+PulseSensorPlayground pulseSensor;
+
+// select the pin for the LED
+
 int Motor1_INT1 = 4;// variable to storethe value coming from the sensor
 int Motor1_INT2 = 5;
 int Motor1_Enable = 3;
-int Pro_input = 2;
-int data_input = 0;
-char ch;
+
+char speedMotor;
+
+
+//char ch;
 void setup() {
   // declare the ledPin as an OUTPUT:
+
+  
   pinMode(Motor1_INT1,OUTPUT);
   pinMode(Motor1_INT2,OUTPUT);
   pinMode(Motor1_Enable,OUTPUT);
-  pinMode(Pro_input,INPUT);
+  //pinMode(PulseWire,INPUT);
 
   digitalWrite(Motor1_INT1,LOW);
   digitalWrite(Motor1_INT2,LOW);
   //digitalWrite(Motor1_Enable,50);
   
-  pinMode(ledPin, OUTPUT);
+  //pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+
+   pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+   if (pulseSensor.begin()) {
+    //Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
+
+  speedMotor = '0';
 }
 
 void loop() {
-  ch = Serial.read();
-  data_input  = digitalRead(Pro_input);
-  sensorValue = analogRead(sensorPin);
+  speedMotor = Serial.read();
+  int myBPM = pulseSensor.getBeatsPerMinute();  
+
+  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
+ //Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+ //Serial.print("BPM: ");                        // Print phrase "BPM: " 
+ Serial.println(myBPM);    // Print the value inside of myBPM.
+  
+  }
+   
+  //ch = Serial.read();
+  //data_input  = digitalRead(Pro_input);
+  //sensorValue = analogRead(sensorPin);
   //Serial.println(data_input);
-  Serial.println(sensorValue,DEC);
-  motorA(ch);
-  analogWrite(Motor1_Enable,sensorValue);
-  delay(100);
+  //Serial.println(sensorValue,DEC);
+  motorA(speedMotor);
+  //analogWrite(Motor1_Enable,sensorValue);
+  delay(20);
 }
 void motorA(char ch){
-   if(ch == 'L'){
-      digitalWrite(Motor1_INT1,HIGH);
-      digitalWrite(Motor1_INT2,LOW);
-      
+  
+    digitalWrite(Motor1_INT1,HIGH);
+    digitalWrite(Motor1_INT2,LOW);
+   if(ch == '0'){
+     analogWrite(Motor1_Enable,0);
    }
-   if(ch == 'R'){
-      digitalWrite(Motor1_INT1,LOW);
-      digitalWrite(Motor1_INT2,HIGH);
-      //digitalWrite(Motor1_Enable,50);
+    if(ch == '1'){
+     analogWrite(Motor1_Enable,50);
+   }
+    if(ch == '2'){
+    analogWrite(Motor1_Enable,100);
+   }
+   if(ch == '3'){
+     analogWrite(Motor1_Enable,150);
+   }
+   if(ch == '4'){
+     analogWrite(Motor1_Enable,200);
    }     
+   if(ch == '5'){
+    analogWrite(Motor1_Enable,250);
+   }
 }
 void button_input(){
      
