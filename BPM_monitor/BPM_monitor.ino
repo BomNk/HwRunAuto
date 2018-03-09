69664644b825a90abd0dfd85a7ee38a9351c882c
Tanapon   Ninket <31594062+BomNk@users.noreply.github.com>
2018-03-09 19:04:23 +0700

    tt

--- BPM_monitor/BPM_monitor.ino
+++ BPM_monitor/BPM_monitor.ino
@@ -13,9 +13,9 @@
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
 //  Variables
-const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+int PulseWire = A0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
 const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
-int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+int Threshold = 500;           // Determine which Signal to "count as a beat" and which to ignore.
                                // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
                                // Otherwise leave the default "550" value. 
                                
@@ -25,7 +25,7 @@ PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPla
 void setup() {   
 
   Serial.begin(9600);          // For Serial Monitor
-
+  pinMode(PulseWire,INPUT);
   // Configure the PulseSensor object, by assigning our variables to it. 
   pulseSensor.analogInput(PulseWire);   
   pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
@@ -41,12 +41,12 @@ void loop() {
 
 int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
                                                // "myBPM" hold this BPM value now. 
-if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
+            // Constantly test to see if "a beat happened". 
  Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
  Serial.print("BPM: ");                        // Print phrase "BPM: " 
  Serial.println(myBPM);    // Print the value inside of myBPM.
   
-}
+
 
   delay(20);                    // considered best practice in a simple sketch.
 
